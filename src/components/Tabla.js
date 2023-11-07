7a2529796981b9773a106bd3469f0618797da816
ZoiloMendoza <zoilomm@hotmail.com>
2023-11-07 15:28:56 -0600

    update

--- src/components/Tabla.js
+++ src/components/Tabla.js
@@ -11,9 +11,9 @@ import CreateIcon from '@mui/icons-material/Create';
 import SaveIcon from '@mui/icons-material/Save';
 import { styled } from '@mui/material/styles';
 const dataInicial = [
-  { SKU: 'X010', MinimoKgsCarga: 1615, 'Inv.BPT + CEDIS dia 5 am': 4077, 'WIP + Programa hoy': 0, '07-nov': 849, 'Inv. Final 1': 3228, '08-nov': 474, 'Inv Final 2': 2754, Tiendita: 2630, Programar: -125, 'Ajuste Cargas': 0, Pedido: 2754, 'Inv final 3': 125, 'Dif Inv final':2754 },
-  { SKU: 'X479', MinimoKgsCarga: 1164, 'Inv.BPT + CEDIS dia 5 am': 559, 'WIP + Programa hoy': 0, '07-nov': 0, 'Inv. Final 1': 559, '08-nov': 0, 'Inv Final 2': 559, Tiendita: 700, Programar: 141, 'Ajuste Cargas': 1, Pedido: 1164, 'Inv final 3': 1723, 'Dif Inv final':1723 },
-  { SKU: 'X971', MinimoKgsCarga: 2568, 'Inv.BPT + CEDIS dia 5 am': 2299, 'WIP + Programa hoy': 0, '07-nov': 854, 'Inv. Final 1': 1445, '08-nov': 760, 'Inv Final 2': 685, Tiendita: 3792, Programar: 3107, 'Ajuste Cargas': 2, Pedido: 5136, 'Inv final 3': 5821, 'Dif Inv final':5821 },
+  { SKU: 'X010', 'Minimo Kgs/carga': 1615, 'Inv.BPT + CEDIS dia 5 am': 4077, 'WIP + Programa hoy': 0, '07-nov': 849, 'Inv. Final 1': 3228, '08-nov': 474, 'Inv Final 2': 2754, Tiendita: 2630, Programar: -125, 'Ajuste Cargas': 0, Pedido: 2754, 'Inv final 3': 125, 'Dif Inv final':2754 },
+  { SKU: 'X479', 'Minimo Kgs/carga': 1164, 'Inv.BPT + CEDIS dia 5 am': 559, 'WIP + Programa hoy': 0, '07-nov': 0, 'Inv. Final 1': 559, '08-nov': 0, 'Inv Final 2': 559, Tiendita: 700, Programar: 141, 'Ajuste Cargas': 1, Pedido: 1164, 'Inv final 3': 1723, 'Dif Inv final':1723 },
+  { SKU: 'X971', 'Minimo Kgs/carga': 2568, 'Inv.BPT + CEDIS dia 5 am': 2299, 'WIP + Programa hoy': 0, '07-nov': 854, 'Inv. Final 1': 1445, '08-nov': 760, 'Inv Final 2': 685, Tiendita: 3792, Programar: 3107, 'Ajuste Cargas': 2, Pedido: 5136, 'Inv final 3': 5821, 'Dif Inv final':5821 },
 ];
 
 const columns = [
@@ -33,10 +33,34 @@ const columns = [
   'Dif Inv final',
 ];
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: '#e2e2e2',
+      color: '#13100c',
+      fontWeight: 700,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+      
+    },
+  }));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(even)': { //Se cambio de odd a even
+      backgroundColor: 'rgba(223, 235, 237, 0.5)',
+      textAlign: 'center', 
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
 
 function Cell({ column, value, isEditing, handleChange, handleEdit, handleSave }) {
+    console.log(value, "cell")
     return (
-      <TableCell>
+      <StyledTableCell align="center">
         {column === 'Ajuste Cargas' ? (
           isEditing ? (
             <div>
@@ -51,23 +75,9 @@ function Cell({ column, value, isEditing, handleChange, handleEdit, handleSave }
         ) : (
           value
         )}
-      </TableCell>
+      </StyledTableCell>
     );
 }
-  
-
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: '#e2e2e2',
-      color: '#13100c',
-      fontWeight: 700,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-
 export default function BasicTable() {
     const [data, setData] = useState(dataInicial);
     const [editingCell, setEditingCell] = useState(null);
@@ -97,14 +107,13 @@ export default function BasicTable() {
           <TableHead>
             <TableRow>
               {columns.map((column) => (
-                <StyledTableCell align="right" key={column}>{column}</StyledTableCell>
-            
+                <StyledTableCell align="center" key={column}>{column}</StyledTableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
             {data.map((row, rowIndex) => (
-              <TableRow key={rowIndex}>
+              <StyledTableRow key={rowIndex}>
                 {columns.map((column) => (
                   <Cell
                     key={column}
@@ -116,13 +125,13 @@ export default function BasicTable() {
                     handleSave={() => handleSaveClick(rowIndex)}
                   />
                 ))}
-              </TableRow>
+              </StyledTableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
     );
-  }
+}
 
 
 
